f3b56c1605bee76511af531ead804daaac138dfa
keronei <keronei95@gmail.com>
2019-03-08 07:39:41 +0300

    Refactored listeners, moved button theming to appTheme component for global control

--- lib/paging/animal_add.dart
+++ lib/paging/animal_add.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 class AddNewAnimal extends StatefulWidget {
   final Function addAnimal;
 
-
   AddNewAnimal(this.addAnimal);
 
   @override
@@ -21,54 +20,79 @@ class _AddNewAnimalState extends State<AddNewAnimal> {
   String description;
   double price_;
 
+  Widget _buildAnimalTitleTextField() {
+    return TextField(
+        onChanged: (String value) {
+          setState(() {
+            AnimalTitle = value;
+          });
+        },
+        decoration: InputDecoration(
+          labelText: "Title",
+        ));
+  }
+
+  Widget _buildAnimalDescriptionTextField() {
+    return TextField(
+      onChanged: (String name) {
+        setState(() {
+          description = name;
+        });
+      },
+      decoration: InputDecoration(labelText: 'Description'),
+    );
+  }
+
+  Widget _buildPriceTextField() {
+    return TextField(
+      keyboardType: TextInputType.number,
+      onChanged: (String price) {
+        setState(() {
+          price_ = double.parse(price);
+        });
+      },
+      decoration: InputDecoration(labelText: 'Price'),
+    );
+  }
+
+  void _submitAnimalData() {
+    final Map<String, dynamic> newAnimal = {
+      'title': AnimalTitle,
+      'desc': description,
+      'price': price_,
+      'imageUrl': 'asset/cat.jpeg'
+    };
+    widget.addAnimal(newAnimal);
+    Navigator.pushReplacementNamed(context, '/land/');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final double mDeviceWidth = MediaQuery.of(context).size.width;
+    final double mTargetWidth = mDeviceWidth > 550.0 ? 500 : mDeviceWidth * 0.95;
+    final double mPadding = mDeviceWidth - mTargetWidth;
+
     return Container(
+
       margin: EdgeInsets.all(10.0),
-      child:  ListView(
-      children: <Widget>[
-        TextField(
-            onChanged: (String value) {
-              setState(() {
-                AnimalTitle = value;
-              });
-            },
-            decoration: InputDecoration(
-              labelText: "Title",
-            )),
-        TextField(
-          onChanged: (String name) {
-           setState(() {
-             description = name;
-           });
-          },
-          decoration: InputDecoration(labelText: 'Description'),
-        ),
-        TextField(
-          keyboardType: TextInputType.number,
-          onChanged: (String price) {
-            setState(() {
-              price_ = double.parse(price);
-            });
-          },
-          decoration: InputDecoration(labelText: 'Price'),
-        )
-        ,
-        SizedBox(height: 10.0,),
-        RaisedButton(
-          color: Theme.of(context).accentColor,
-          textColor: Colors.white,
-          child: Text('Save'), onPressed: (){
-        final Map<String, dynamic> newAnimal = {
-          'title': AnimalTitle,
-          'desc': description,
-          'price':price_,
-          'imageUrl':'asset/cat.jpeg'
-        };
-          widget.addAnimal(newAnimal);
-          Navigator.pushReplacementNamed(context, '/');
-        },)
-      ],
-    ),);
+      child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: mPadding /2 ),
+        children: <Widget>[
+          _buildAnimalTitleTextField(),
+          _buildAnimalDescriptionTextField(),
+          _buildPriceTextField(),
+          SizedBox(
+            height: 10.0,
+          ),
+          RaisedButton(
+
+            textColor: Colors.white,
+            child: Text('Save'),
+            onPressed: _submitAnimalData,
+          )
+        ],
+      ),
+    );
   }
 }

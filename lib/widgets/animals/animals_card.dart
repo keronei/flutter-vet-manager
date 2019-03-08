607b55e38cd50b6e95bea7fedb8482d28550b101
keronei <keronei95@gmail.com>
2019-03-08 07:46:58 +0300

    Refactored widgets(ButtonBars and Text)

---
+++ lib/widgets/animals/animals_card.dart
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2019. Created by keronei lincoln, All rights reserved.
+ */
+
+import 'package:flutter/material.dart';
+import 'price_tag.dart';
+import '../ui_elements/title.dart';
+import './address_tag.dart';
+
+class AnimalCard extends StatelessWidget {
+  final Map<String, dynamic> animal;
+
+  final int animalIndex;
+
+  const AnimalCard(this.animal, this.animalIndex);
+  Widget _buldTitleAndTag(){
+    return      Container(
+        margin: EdgeInsets.only(top: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TitleDefault(animal['title']),
+            SizedBox(
+              width: 10.0,
+            ),
+            PriceTag(animal["price"].toString())
+          ],
+        ));
+  }
+  ButtonBar _builtButtons(BuildContext context){
+    return ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          color: Theme.of(context).accentColor,
+          icon: Icon(Icons.details),
+          onPressed: () {
+            Navigator.pushNamed<String>(
+                context, '/animal/' + animalIndex.toString());
+          },
+        ),
+        IconButton(
+            icon: Icon(Icons.favorite_border),
+            color: Colors.red,
+            onPressed: () {}),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(animal['imageUrl']),
+     _buldTitleAndTag()
+     ,
+         AddressTag('Trans-Zoia, Ke'),
+          _builtButtons(context),
+        ],
+      ),
+    );
+    ;
+  }
+}

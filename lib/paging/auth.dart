5eca5f0efe0eab286bfebf903ced1e3570a593c3
keronei <keronei95@gmail.com>
2019-03-08 07:41:15 +0300

    Recalculated layout with for custom sizes(and orientation)

--- lib/paging/auth.dart
+++ lib/paging/auth.dart
@@ -17,60 +17,97 @@ class _AuthState extends State<AuthPage> {
   String user_password;
   bool terms_and_conditions = false;
 
+
+
+  DecorationImage _decorationImageBuilder(){
+   return  DecorationImage(
+        fit: BoxFit.cover,
+        colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.3), BlendMode.dstATop),
+        image: AssetImage('asset/catlogin.jpg'));
+  }
+
+  Widget _userEmailTF(){
+    return  TextField(
+      decoration: InputDecoration(
+          labelText: "Email",
+          filled: true, fillColor: Colors.white
+      ),
+      keyboardType: TextInputType.emailAddress,
+      onChanged: (String musermail) {
+        setState(() {
+          user_mail = musermail;
+        });
+      },
+    );
+  }
+
+  Widget _userPasswordTF(){
+    return   TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          labelText: "Password",
+          filled: true, fillColor: Colors.white
+      ),
+      onChanged: (String muserpass) {
+        setState(() {
+          user_password = muserpass;
+        });
+      },
+    );
+  }
+  Widget _buildSwitchTile(){
+    return    SwitchListTile(
+      value: terms_and_conditions,
+      onChanged: (bool Value) {
+        setState(() {
+          terms_and_conditions = Value;
+        });
+      },
+      title: Text('Agree to T & C'),
+    );
+  }
+
+  void _submitLogin() {
+
+    Navigator.pushReplacementNamed(context, '/land');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final double mDeviceWidth = MediaQuery.of(context).size.width;
+    final double mTargetWidth = mDeviceWidth > 768.0 ? 500 : mDeviceWidth * 0.95;
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Account"),
       ),
       body: Container(
+        decoration: BoxDecoration(
+            image:_decorationImageBuilder()),
         padding: EdgeInsets.all(10.0),
-        child: Column(
+       child: Center(
+
+       child: SingleChildScrollView(
+        child: Container(
+          width:mTargetWidth,
+          child: Column(
           children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Email",
-              ),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String musermail) {
-                setState(() {
-                  user_mail = musermail;
-                });
-
-              },
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Password",
-              ),
-              onChanged: (String muserpass) {
-                setState(() {
-                  user_password = muserpass;
-                });
-
-              },
-            ),
-            SwitchListTile(
-              value: terms_and_conditions,
-              onChanged: (bool Value) {
-                setState(() {
-                  terms_and_conditions = Value;
-                });
-              },
-              title: Text('Agree to T & C'),
-            ),
+           _userEmailTF(),
+            SizedBox(height: 10.0,),
+          _userPasswordTF()
+          ,_buildSwitchTile()
+         ,
             RaisedButton(
                 child: Text('login'),
-                color: Theme.of(context).primaryColor,
                 textColor: Colors.white,
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/');
-                })
+                onPressed: _submitLogin,)
           ],
         ),
-      ),
-    );
+      ),),
+    ),),);
     ;
   }
 }

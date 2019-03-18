e1855949979f942a495b757ade94d0a1a4514cb8
keronei <keronei95@gmail.com>
2019-03-18 06:00:44 +0300

    implememnted the product class to replace Maps

--- lib/paging/animal_edit.dart
+++ lib/paging/animal_edit.dart
@@ -4,15 +4,17 @@
 
 import 'package:flutter/material.dart';
 import '../widgets/helpers/ensure-visible.dart';
+import '../models/animal.dart';
 
 class editAnimal extends StatefulWidget {
   final Function addAnimal;
   final Function updateAnimal;
   final int animalIndex;
 
-  final Map<String, dynamic> singleAnimal;
+  final Animal singleAnimal;
 
-  editAnimal({this.addAnimal, this.updateAnimal, this.singleAnimal, this.animalIndex});
+  editAnimal(
+      {this.addAnimal, this.updateAnimal, this.singleAnimal, this.animalIndex});
 
   @override
   State<StatefulWidget> createState() {
@@ -36,10 +38,9 @@ class _editAnimalState extends State<editAnimal> {
   final _priceFocusNode = FocusNode();
 
   Widget pageStructureContent() {
-
     final double mDeviceWidth = MediaQuery.of(context).size.width;
     final double mTargetWidth =
-    mDeviceWidth > 550.0 ? 500 : mDeviceWidth * 0.95;
+        mDeviceWidth > 550.0 ? 500 : mDeviceWidth * 0.95;
     final double mPadding = mDeviceWidth - mTargetWidth;
 
     return GestureDetector(
@@ -70,67 +71,69 @@ class _editAnimalState extends State<editAnimal> {
       ),
     );
   }
+
   Widget _buildAnimalTitleTextField() {
     return EnsureVisibleWhenFocused(
       focusNode: _titleFocusNode,
-      child:
-        TextFormField(
+      child: TextFormField(
           focusNode: _titleFocusNode,
-        initialValue:
-            widget.singleAnimal == null ? '' : widget.singleAnimal['title'],
-        validator: (String titleProvided) {
-          if (titleProvided.isEmpty || titleProvided.length < 5) {
-            return 'Title is reuired, 5 characters min';
-          }
-        },
-        onSaved: (String value) {
-          animalAddForm['title'] = value;
-        },
-        decoration: InputDecoration(
-          labelText: "Title",
-        )),);
+          initialValue:
+              widget.singleAnimal == null ? '' : widget.singleAnimal.title,
+          validator: (String titleProvided) {
+            if (titleProvided.isEmpty || titleProvided.length < 5) {
+              return 'Title is reuired, 5 characters min';
+            }
+          },
+          onSaved: (String value) {
+            animalAddForm['title'] = value;
+          },
+          decoration: InputDecoration(
+            labelText: "Title",
+          )),
+    );
   }
 
   Widget _buildAnimalDescriptionTextField() {
     return EnsureVisibleWhenFocused(
+      focusNode: _descFocusNode,
+      child: TextFormField(
         focusNode: _descFocusNode,
-        child:
-        TextFormField(
-        focusNode: _descFocusNode,
-      initialValue:
-          widget.singleAnimal == null ? '' : widget.singleAnimal['desc'],
-      validator: (String animalName) {
-        if (animalName.isEmpty || animalName.length < 6) {
-          return 'Description required, 6 characters min';
-        }
-      },
-      onSaved: (String name) {
-        animalAddForm['desc'] = name;
-      },
-      decoration: InputDecoration(labelText: 'Description'),
-    ),);
+        initialValue:
+            widget.singleAnimal == null ? '' : widget.singleAnimal.desc,
+        validator: (String animalName) {
+          if (animalName.isEmpty || animalName.length < 6) {
+            return 'Description required, 6 characters min';
+          }
+        },
+        onSaved: (String name) {
+          animalAddForm['desc'] = name;
+        },
+        decoration: InputDecoration(labelText: 'Description'),
+      ),
+    );
   }
 
   Widget _buildPriceTextField() {
     return EnsureVisibleWhenFocused(
+      focusNode: _priceFocusNode,
+      child: TextFormField(
         focusNode: _priceFocusNode,
-        child:
-        TextFormField(
-        focusNode: _priceFocusNode,
-      initialValue:
-          widget.singleAnimal == null ? '' : widget.singleAnimal['price'].toString(),
-      validator: (String inputPrice) {
-        if (inputPrice.isEmpty ||
-            !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(inputPrice)) {
-          return ' Price should be a number';
-        }
-      },
-      keyboardType: TextInputType.number,
-      onSaved: (String price) {
-        animalAddForm['price'] = double.parse(price);
-      },
-      decoration: InputDecoration(labelText: 'Price'),
-    ),);
+        initialValue: widget.singleAnimal == null
+            ? ''
+            : widget.singleAnimal.price.toString(),
+        validator: (String inputPrice) {
+          if (inputPrice.isEmpty ||
+              !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(inputPrice)) {
+            return ' Price should be a number';
+          }
+        },
+        keyboardType: TextInputType.number,
+        onSaved: (String price) {
+          animalAddForm['price'] = double.parse(price);
+        },
+        decoration: InputDecoration(labelText: 'Price'),
+      ),
+    );
   }
 
   void _submitAnimalData() {
@@ -139,19 +142,25 @@ class _editAnimalState extends State<editAnimal> {
     }
     _globalKey.currentState.save();
 
-    if(widget.singleAnimal == null){
+    if (widget.singleAnimal == null) {
+      widget.addAnimal(Animal(
+          title: animalAddForm['title'],
+          desc: animalAddForm['desc'],
+          price: animalAddForm['price'],
+          imageUrl: animalAddForm['imageUrl']));
 
-    widget.addAnimal(animalAddForm);
-    Navigator.pushReplacementNamed(context, '/land/');}
-    else{
-      widget.updateAnimal(widget.animalIndex, animalAddForm);
+    } else {
+      widget.updateAnimal(widget.animalIndex, Animal(
+          title: animalAddForm['title'],
+          desc: animalAddForm['desc'],
+          price: animalAddForm['price'],
+          imageUrl: animalAddForm['imageUrl']));
     }
+    Navigator.pushReplacementNamed(context, '/land/');
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     Widget pageStructure = pageStructureContent();
 
     return widget.singleAnimal == null

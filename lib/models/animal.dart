e3c633a1937c028c3967dcce3a6fbb77cba6ac98
keronei <keronei95@gmail.com>
2019-03-18 05:58:55 +0300

    Introduced class instance for managing data

---
+++ lib/models/animal.dart
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019. Created by keronei lincoln, All rights reserved.
+ */
+import 'package:flutter/material.dart';
+
+class Animal{
+  final String title;
+  final String desc;
+  final double price;
+  final String imageUrl;
+
+  Animal({@required this.title, @required this.desc, @required this.price, @required this.imageUrl});
+}

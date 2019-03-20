b724f752c1970f4825acec4769538b717e48a8e2
keronei <keronei95@gmail.com>
2019-03-20 06:37:45 +0300

    implemented scope model usage

--- lib/paging/animal.dart
+++ lib/paging/animal.dart
@@ -2,18 +2,18 @@
  * Copyright (c) 2019. Created by keronei lincoln, \n All rights reserved.
  */
 import 'dart:async';
+import 'package:scoped_model/scoped_model.dart';
 import 'package:flutter/material.dart';
 import '../widgets/ui_elements/title.dart';
 import '../widgets/animals/address_tag.dart';
 import '../widgets/animals/price_tag.dart';
+import '../scoped_models/animals.dart';
+import '../models/animal.dart';
 
 class AnimalPage extends StatelessWidget {
-  final String title;
-  final String imageUrl;
-  final double price;
-  final String animalDescription;
+  final int mAnimalIndex;
 
-  AnimalPage(this.title, this.imageUrl, this.price, this.animalDescription);
+  AnimalPage(this.mAnimalIndex);
 
   _ShowWarningDialog(BuildContext context) {
     showDialog(
@@ -41,7 +41,7 @@ class AnimalPage extends StatelessWidget {
         });
   }
 
-  Widget _buildAddressPriceRow() {
+  Widget _buildAddressPriceRow(double price) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
@@ -68,37 +68,45 @@ class AnimalPage extends StatelessWidget {
     return WillPopScope(
       onWillPop: () {
         Navigator.pop(context, "dont");
-        print("Sprint with false");
+
         return Future.value(false);
       },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(imageUrl),
-            Container(padding: EdgeInsets.all(9.0), child: TitleDefault(title)),
-            _buildAddressPriceRow(),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                animalDescription,
-                textAlign: TextAlign.center,
+      child: ScopedModelDescendant<AnimalsModel>(builder: (BuildContext context, Widget child, AnimalsModel model){
+
+        final List<Animal> animals = model.animals;
+
+        Animal singleAnimal = animals[mAnimalIndex];
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(singleAnimal.title),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(singleAnimal.imageUrl),
+              Container(padding: EdgeInsets.all(9.0), child: TitleDefault(singleAnimal.title)),
+              _buildAddressPriceRow(singleAnimal.price),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  singleAnimal.desc,
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(9.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('remove'),
-                onPressed: () => _ShowWarningDialog(context),
-              ),
-            )
-          ],
-        ),
-      ),
+              Container(
+                padding: EdgeInsets.all(9.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('remove'),
+                  onPressed: () => _ShowWarningDialog(context),
+                ),
+              )
+            ],
+          ),
+        );
+
+      },),
     );
   }
 }

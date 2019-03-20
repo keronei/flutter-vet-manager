e4515f13a1589435bfbaec279991acc286b258a7
keronei <keronei95@gmail.com>
2019-03-20 07:54:13 +0300

    Introduced user instance

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019. Created by keronei lincoln, All rights reserved.
+ */
+
+import 'package:flutter/material.dart';
+
+class User {
+  final String mId;
+  final String mEmail;
+  final String mUserPassword;
+
+  User({@required this.mId, @required this.mEmail, @required this.mUserPassword});
+}
